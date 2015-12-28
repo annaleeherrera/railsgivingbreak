e1abd2b326adc0eff9ec9fc0b5530ec1922923da
Desiree <desireepoland@gmail.com>
2015-12-28 13:47:32 -0800

    regenerate model specs, add spec for recipes controller get#index

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
+  describe "GET 'index'" do
+    it "is successful" do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
 
 end
