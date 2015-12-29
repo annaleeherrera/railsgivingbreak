ead9b3a98f61cb3775baf8d7740f0376bc818ee7
Sarah <trowbrsa@gmail.com>
2015-12-29 12:50:50 -0800

    Created CRUD for people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,6 +13,33 @@ class PeopleController < ApplicationController
     @person = Person.find(id)
   end
 
+  def new
+    @title = "Add a Chef"
+    @person = Person.new
+    @action = :create
+  end
+
+  def create
+    @person = Person.create(people_params)
+    redirect_to people_path
+  end
+
+  def edit
+    @title = "Edit"
+    @person = Person.find(params[:id])
+    @action = :update
+  end
+
+  def update
+    Person.update(params[:id], people_params)
+    redirect_to people_path
+  end
+
+  def destroy
+    Person.destroy(params[:id])
+    redirect_to people_path
+  end
+
 private
   def people_params
     params.require(:person).permit(:name)
